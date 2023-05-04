5eb2a2fbc2d6795edd343f73179a200a0b5355a5
Moustapha Amadou Roufaï <moustapharouf@yahoo.fr>
2023-05-04 20:33:09 +0100

    Update 2_Project_Team.py

--- pages/2_Project_Team.py
+++ pages/2_Project_Team.py
@@ -55,7 +55,7 @@ PAGE_TITLE="Diagnose Sickle Cell Diseases by Red Blood Cells(RBCs) Classificatio
 HOMEPAGE_CHAPTERLEAD='''
 | Chapter Name | Lead Name |
 |--|--|
-| Benin Chapter Leads | Moustapha Amadou Roufaï |
+| Benin Chapter Leads | Moustapha Amadou Roufaï - moustapharouf@yahoo.fr |
 '''
 
 HOMEPAGE_ACTIVE_MEMBERS='''
